5a4ef00e3e89ac534321b41faec249a7e0f0aec7
natuliochek <natvicor@mail.ru>
2025-05-20 18:48:25 +0200

    worked css

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react';
 import { useEffect } from 'react';
+import pictureUp from './faultier.png';
+import pictureDown from './relax.png';
 import './App.css';
 
 function App () {
@@ -20,15 +22,17 @@ function App () {
     <div className='main-box'>
       <div className='box'>
         <h1>When you feel lazy and bored...  </h1>
-        <h2>and you are thinking, what else you don´t want to do,</h2>
-        <h2>let´s see, what could be randomly offered...</h2>
       </div>
+      <img className='picUp' src={pictureUp} width="150px" />
+      <div className='par'>
+        <p>and you are thinking, what else you don´t want to do,</p>
+        <p>let´s see, what could be randomly offered...</p>
+      </div>
+      <button onClick={getAdvice}>press here</button>
       <div className='box'>
-        <button onClick={getAdvice}>press here</button>
+        <h2>"{advice}"</h2>
       </div>
-      <div className='box'>
-        <p>"{advice}"</p>
-      </div>
+      <img className='picDown' src={pictureDown} width="150px" />
     </div>
   )
 }
